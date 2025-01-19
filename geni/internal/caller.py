66607661814d73712e775e0c5678b71fa83b2c65
bryndin <1129396+bryndin@users.noreply.github.com>
2025-01-18 20:57:05 -0800

    refactor Caller class and add tests

---
+++ geni/internal/caller.py
@@ -0,0 +1,59 @@
+import requests
+
+from geni.internal.auth import Auth
+from geni.internal.ratelimiter import RateLimiter
+
+
+def remove_none(d):
+    """Remove None values from a dictionary."""
+    return {k: v for k, v in d.items() if v is not None}
+
+def flatten_dict(d, parent_key=""):
+    items = {}
+    for k, v in d.items():
+        new_key = f"{parent_key}[{k}]" if parent_key else k
+        if isinstance(v, dict):
+            items.update(flatten_dict(v, new_key))
+        else:
+            items[new_key] = v
+    return items
+
+class Caller:
+    __HEADER_AUTH = "Authorization"
+
+    def __init__(self, api_key=None):
+        self._auth = Auth(api_key)
+        self._ratelimiter=RateLimiter()
+
+    def _call(self, url, headers=None, params=None, method="get"):
+        """
+        Preprocess params before executing request.
+        """
+        # Flatten dicts passed inside params
+        processed_params = {}
+        for k, v in params.items():
+            if isinstance(v, dict):
+                processed_params.update(flatten_dict(v, parent_key=k))
+            else:
+                processed_params[k] = v
+
+        # Remove None values
+        processed_params = remove_none(processed_params)
+
+        return self._raw_call(url, headers=headers, params=processed_params, method=method)
+
+    def _raw_call(self, url, headers=None, params=None, method="get"):
+        """
+        Execute request with auth and ratelimiting.
+        """
+        headers = headers or {}
+        if self.__HEADER_AUTH not in headers:
+          headers[self.__HEADER_AUTH] = f"Bearer {self._auth.access_token}"
+
+        params = params or {}
+
+        self._ratelimiter.wait()
+        response = requests.request(method, url, headers=headers, params=params)
+        self._ratelimiter.update(response.headers)
+
+        return response
---
+++ tests/test_caller.py
@@ -0,0 +1,126 @@
+import json
+from collections import deque
+from unittest.mock import Mock, call, patch, MagicMock
+
+import pytest
+import requests
+
+from geni.internal.auth import Auth
+from geni.internal.caller import Caller, flatten_dict, remove_none
+from geni.internal.ratelimiter import RateLimiter
+from tests.internal.fixtures import sampleProfile
+
+
+@pytest.mark.parametrize(
+    "d, expect",
+    [
+        ({}, {}),
+        ({"a": None, "b": "c", "d": None}, {"b": "c"}),
+    ]
+)
+def test_remove_none(d, expect):
+    assert remove_none(d) == expect
+
+
+def test_remove_none_returns_a_dict_copy():
+    d = {"a": 1, "b": 2}
+    result = remove_none(d)
+    d.pop("b")
+    assert result == {"a": 1, "b": 2}
+    assert d != result
+
+
+@pytest.mark.parametrize(
+    "d, parent_key, expect",
+    [
+        pytest.param({}, "", {},
+                     id="empty dict => empty dict"),
+
+        pytest.param({}, "key", {},
+                     id="empty dict with a parent key => empty dict"),
+
+        pytest.param(sampleProfile["names"], "", {
+            "en[first_name]": sampleProfile["names"]["en"]["first_name"],
+            "en[last_name]": sampleProfile["names"]["en"]["last_name"],
+            "es[first_name]": sampleProfile["names"]["es"]["first_name"],
+            "es[last_name]": sampleProfile["names"]["es"]["last_name"],
+        }, id="names dict with no parent key => flattened dict"),
+
+        pytest.param(sampleProfile["names"], "names", {
+            "names[en][first_name]": sampleProfile["names"]["en"]["first_name"],
+            "names[en][last_name]": sampleProfile["names"]["en"]["last_name"],
+            "names[es][first_name]": sampleProfile["names"]["es"]["first_name"],
+            "names[es][last_name]": sampleProfile["names"]["es"]["last_name"],
+        }, id="names dict with 'names' as a parent key => flattened dict with 'names' as a parent key"),
+    ],
+)
+def test_flatten_dict(d, parent_key, expect):
+    assert flatten_dict(d, parent_key) == expect
+
+
+def test_flatten_dict_returns_a_dict_copy():
+    d = {"key": "value"}
+    result = flatten_dict(d)
+    d.pop("key")
+    assert result == {"key": "value"}
+    assert d != result
+
+
+def test___init__():
+    caller = Caller(api_key="test_key")
+
+    assert isinstance(caller._auth, Auth)
+    assert isinstance(caller._ratelimiter, RateLimiter)
+    assert caller._auth._api_key == "test_key"
+
+
+def test__call():
+    expected_response = json.dumps("test_response")
+
+    with patch.object(Caller, "_raw_call", return_value=expected_response) as mock___call:
+        caller = Caller(api_key="test_key")
+
+        url = "test_url"
+        headers = {"test_header": "test_value"}
+        params = {"p": "v", "none": None, "dict": {"key": "val"}}
+        method = "test_method"
+        response = caller._call(url, headers=headers, params=params, method=method)
+
+        assert response == expected_response
+        mock___call.assert_called_once_with(url, headers=headers, params={"p": "v", "dict[key]": "val"}, method=method)
+
+
+@pytest.mark.parametrize(
+    "url, kwargs, "
+    "access_token,"
+    "expect_method, expect_headers, expect_params",
+    [
+        pytest.param("https://api.com/a", {},
+                     "TOKEN_A",
+                     "get", {"Authorization": "Bearer TOKEN_A"}, {},
+                     id="get with no headers no params => success"),
+        pytest.param("https://api.com/b", {"headers": {"Custom-Header": "value"}, "params": {"key": "value"}, "method": "post"},
+                     "TOKEN_B",
+                     "post", {"Custom-Header": "value", "Authorization": "Bearer TOKEN_B"}, {"key": "value"},
+                     id="post with headers and params => success"),
+    ]
+)
+def test__raw_call(url, kwargs, access_token, expect_method, expect_headers, expect_params):
+    mock_response = requests.Response()
+    mock_response.headers = {"X-API-Rate-Limit": "100"}
+
+    mock_access_token = MagicMock(return_value=access_token)
+
+    with patch("requests.request", return_value=mock_response) as mock_request, \
+            patch.object(RateLimiter, "wait") as mock_wait, \
+            patch.object(RateLimiter, "update") as mock_update, \
+            patch.object(Auth, "access_token", new_callable=mock_access_token):
+
+        caller = Caller(api_key="dummy-api-key")
+        response = caller._raw_call(url, **dict(kwargs))
+
+        assert response == mock_response
+        mock_access_token.assert_called_once()
+        mock_wait.assert_called_once()
+        mock_request.assert_called_once_with(expect_method, url, headers=expect_headers, params=expect_params)
+        mock_update.assert_called_once_with(mock_response.headers)
